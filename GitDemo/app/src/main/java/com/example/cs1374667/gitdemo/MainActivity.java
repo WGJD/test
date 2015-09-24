f9fa6f6a14939151a32f4eca69767c793fc4c561
Jack David <jack_jp_david@hotmail.com>
2015-09-24 12:24:56 -0400

    Added "Press Me" Button
    Added to layout and main activity

--- GitDemo/app/src/main/java/com/example/cs1374667/gitdemo/MainActivity.java
+++ GitDemo/app/src/main/java/com/example/cs1374667/gitdemo/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button pressMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
